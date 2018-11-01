ed4a3b9a343e051c93af6074582c9723e431ecb6
Adam <mradamhimself@gmail.com>
2018-11-01 11:24:19 +0100

    Converted project from bootstrap 3 -> 4. Simplified styles / scripts references.

--- DutchApp/wwwroot/dist/vendor.bundle.js
+++ DutchApp/wwwroot/dist/vendor.bundle.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b42f6708d06110ecbbda";
+/******/ 	var hotCurrentHash = "7ab7d426800b74ff7b98";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
